13474412597c9c3aa6917337aa57e1b5a46a8b60
renjithcg <renjithcg09@gmail.com>
2018-05-03 15:03:22 +0530

    Python program to print the string

---
+++ Assignement_2.4.py
@@ -0,0 +1,4 @@
+print("""WE, THE PEOPLE OF INDIA,
+\thaving solemnly resolved to constitute India into a SOVEREIGN, 
+\t\t   SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC 
+ \t\t    and to secure to all its citizens""")
