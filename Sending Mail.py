31a7701d78822c9053939147b024671867742afd
renjithcg <34932203+renjithcg@users.noreply.github.com>
2018-05-09 15:56:55 +0530

    Delete Sending Mail.py

--- Sending Mail.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-s = smtplib.SMTP('smtp.gmail.com',587)
-s.ehlo()
-s.starttls()
-s.login("username","password")
-msg = "Hello This is a test message"
-s.sendmail("fromaddress","toaddress",msg)
